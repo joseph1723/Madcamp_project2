9dc97fce1b4baa00978b621ba888f11a2f0a4a29
kimdonggeun <bluetiger1723@gmail.com>
2024-07-07 02:10:43 +0900

    basic frontline implemented

--- lib/sample_screen.dart
+++ lib/sample_screen.dart
@@ -1,13 +1,11 @@
-//import 'dart:convert';
-
 import 'package:flutter/material.dart';
-//import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
 import 'package:goggle_login/login_platform.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-//import 'package:http/http.dart' as http;
+import 'tab1_screen.dart'; // tab1 스크린을 정의한 파일을 import합니다.
+import 'google_map_screen.dart'; // google_map_screen을 import합니다.
 
 class SampleScreen extends StatefulWidget {
-  const SampleScreen({super.key});
+  const SampleScreen({Key? key}) : super(key: key);
 
   @override
   State<SampleScreen> createState() => _SampleScreenState();
@@ -44,21 +42,49 @@ class _SampleScreenState extends State<SampleScreen> {
     });
   }
 
+  void navigateToTab1() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => Tab1Screen()), // Tab1Screen으로 이동합니다.
+    );
+  }
+
+  void navigateToGoogleMapScreen() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => GoogleMapScreen()), // GoogleMapScreen으로 이동합니다.
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Sample Screen'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.tab), // 탭 아이콘 사용 가능
+            onPressed: navigateToTab1, // 탭1로 이동하는 함수 호출
+          ),
+          IconButton(
+            icon: Icon(Icons.map), // 지도 아이콘 사용 가능
+            onPressed: navigateToGoogleMapScreen, // Google 지도로 이동하는 함수 호출
+          ),
+        ],
+      ),
       body: Center(
-          child: _loginPlatform != LoginPlatform.none
-              ? _logoutButton()
-              : Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    _loginButton(
-                      'google',
-                      signInWithGoogle,
-                    )
-                  ],
-                )),
+        child: _loginPlatform != LoginPlatform.none
+            ? _logoutButton()
+            : Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _loginButton(
+              'google',
+              signInWithGoogle,
+            )
+          ],
+        ),
+      ),
     );
   }
 
@@ -85,7 +111,7 @@ class _SampleScreenState extends State<SampleScreen> {
     return ElevatedButton(
       onPressed: signOut,
       style: ButtonStyle(
-        backgroundColor: WidgetStateProperty.all(
+        backgroundColor: MaterialStateProperty.all(
           const Color(0xff0165E1),
         ),
       ),
